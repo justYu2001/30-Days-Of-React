51b72b528e4610e0c9ca6f6faaefa8c592da70ca
justYu2001 <jack4781012@gmail.com>
2022-08-15 19:08:54 +0800

    update day16

--- solutions/day-16/src/components/App/App.jsx
+++ solutions/day-16/src/components/App/App.jsx
@@ -1,43 +1,48 @@
 import React from 'react';
 
-const inputCanHandleAllType = (InputComponent, callback) => {
-    const handleChange = (event) => {
-        const { name, type, value, checked } = event.target;
+const inputCanHandleAllType = (InputComponent) => {
+    return (props) => {
+        const { onChange: callback , ...restProps } = props;
 
-        if (type === 'checkbox') {
-            callback(name, checked);
-        } else if (type === 'file') {
-            callback(name, event.target.files[0]);
-        } else {
-            callback(name, value);
-        }
+        const handleChange = (event) => {
+            const { name, type, value, checked } = event.target;
+    
+            if (type === 'checkbox') {
+                callback(name, checked);
+            } else if (type === 'file') {
+                callback(name, event.target.files[0]);
+            } else {
+                callback(name, value);
+            }
+        };
+
+        return (
+            <InputComponent {...restProps} onChange={handleChange} />
+        );
     };
-
-    return (props) => <InputComponent {...props} onChange={handleChange} />;
 };
 
-const Input = ({ type, name, onChange }) => {
-     const styles = {
-        padding: '5px 10px',
-        outline: 'none'
-     };
-    
+const Input = ({ type, name, onChange }) => {    
     return (
-        <input type={type} name={name} style={styles} onChange={onChange} />
+        <input type={type} name={name} onChange={onChange} />
     );
 }
 
-const TextInput = inputCanHandleAllType(Input, (name, value) => {
-    console.log(name, value);
-});
+const SuperInput = inputCanHandleAllType(Input);
 
 
 const App = () => {
-  return (
-    <div>
-      <TextInput type='text' name='name' />
-    </div>
-  );
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+            <SuperInput type='text' name='name' onChange={(name, value) => console.log(name, value)} />
+            <SuperInput type='checkbox' name='checkbox' onChange={(name, value) => console.log(name, value)} /> 
+            <SuperInput type='date' name='date' onChange={(name, value) => console.log(name, value)} />
+            <SuperInput type='range' name='range' onChange={(name, value) => console.log(name, value)} />
+            <SuperInput type='email' name='email' onChange={(name, value) => console.log(name, value)} />
+            <SuperInput type='color' name='color' onChange={(name, value) => console.log(name, value)} />
+            <SuperInput type='reset' name='reset' onChange={(name, value) => console.log(name, value)} />
+        </div>
+    );
 };
 
 export default App;
